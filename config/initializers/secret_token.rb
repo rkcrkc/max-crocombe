5e14af24fbbf5d33c062b25b21fff3bb9dd84bb3
Ryan Crocombe <ryancrocombe@gmail.com>
2014-07-13 17:34:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MaxCrocombe::Application.config.secret_key_base = '14e780d8ce299ef2b1be4f6060351fbdc37e1a6954febf02d1872c5b725f8e55c5a0a3e0324636650242c5003b26b1fb216bbb01d1998679e64ec7555c878915'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MaxCrocombe::Application.config.session_store :cookie_store, key: '_max-crocombe_session'
